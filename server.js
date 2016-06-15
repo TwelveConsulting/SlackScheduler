55a1f2b3b72a81adf0ea1dbb7dba212b61208f20
mgeier82 <mgeier82@gmail.com>
2016-06-15 16:48:54 +0200

    init

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     conges = require('./modules/timesheet'),
+    schedule = require('node-schedule'),
     app = express();
 
 app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-//app.post('/conges', conges.execute);
+var j = schedule.scheduleJob('42 * * * *', function(){
+  console.log('The answer to life, the universe, and everything!');
+});
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
     //auth.login();
-});
-
-var schedule = require('node-schedule');
- 
-var j = schedule.scheduleJob('42 * * * *', function(){
-  console.log('The answer to life, the universe, and everything!');
 });
