d596dde35e43961be0f689012dbc9f982337ef9c
mgeier82 <mgeier82@gmail.com>
2016-06-15 17:03:17 +0200

    init

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-var j = schedule.scheduleJob('42 * * * *', function(){
-  console.log('The answer to life, the universe, and everything!');
-});
-
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
     //auth.login();
