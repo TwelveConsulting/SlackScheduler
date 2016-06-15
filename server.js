afbe74dce69e1742b14005dfc302a8ea2e53ff27
mgeier82 <mgeier82@gmail.com>
2016-06-15 16:44:29 +0200

    init

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ var express = require('express'),
     conges = require('./modules/timesheet'),
     app = express();
 
+app.set('port', process.env.PORT || 5000);
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+//app.post('/conges', conges.execute);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+    //auth.login();
+});
 
 var schedule = require('node-schedule');
  
